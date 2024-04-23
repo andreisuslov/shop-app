e0f9831d519b654e23b027a0a1efc9d35b49780a
andreisuslov <truvord@gmail.com>
2024-04-23 15:37:06 -0400

    Add react-router and update navigation links

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
 import logo from '../assets/logo.png';
 
 const Header = () => {
@@ -7,27 +8,33 @@ const Header = () => {
 		<header>
 			<Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
 				<Container>
-					<Navbar.Brand href="/">
-						<img
-							src={logo}
-							alt="ProShop"
-							width="30"
-							height="30"
-							className="d-inline-block align-top"
-						/>
-						ProShop
-					</Navbar.Brand>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>
+                            <img
+                                src={logo}
+                                alt="ProShop"
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                            />
+                            ProShop
+                        </Navbar.Brand>
+					</LinkContainer>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="ms-auto">
-							<Nav.Link href="/cart">
-								<FaShoppingCart />
-								&nbsp;Cart
-							</Nav.Link>
-							<Nav.Link href="/login">
-								<FaUser />
-								&nbsp;Login
-							</Nav.Link>
+							<LinkContainer to="/cart">
+                                <Nav.Link>
+                                    <FaShoppingCart />
+                                    &nbsp;Cart
+                                </Nav.Link>
+                            </LinkContainer>
+							<LinkContainer to="/login">
+                                <Nav.Link>
+                                    <FaUser />
+                                    &nbsp;Login
+                                </Nav.Link>
+                            </LinkContainer>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider
+} from "react-router-dom";
 import './assets/index.css';
 import './assets/bootstrap.custom.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screens/HomeScreen';
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<App />}>
+			{/* 
+			HomeScreen is the default route, so we can omit the index prop
+			index={true} means that this is the default route
+			path="/" means that this route is the root route
+			element={<HomeScreen />} means that this route will render the HomeScreen component 
+			*/}
+			<Route index={true} path="/" element={<HomeScreen />} />
+		</Route>
+	)
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>,
 );
 
