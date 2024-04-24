847274470b8b4440cd05cb61d56134fa3d40b943
andreisuslov <truvord@gmail.com>
2024-04-24 15:33:34 -0400

    Add MongoDB connection and update dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 import products from './data/products.js';
 import net from 'net';
+
+connectDB();
 
 const app = express();
 
