14e82bd1821ea0a2c707846f525b73b34b6fed41
andreisuslov <truvord@gmail.com>
2024-04-24 12:37:20 -0400

    Add products data and API routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import http from 'http';
+import products from './data/products.js';
 import net from 'net';
 
 const app = express();
@@ -8,6 +8,15 @@ const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('API is running...');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
 });
 
 const findAvailablePort = (basePort, callback) => {
