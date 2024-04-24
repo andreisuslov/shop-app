6382271599f516d45d3e64f8d1ea999d45f32af2
andreisuslov <truvord@gmail.com>
2024-04-24 17:00:59 -0400

    Add asyncHandler and update product routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,16 +1,31 @@
-import express from "express";
+import express from 'express';
 
 const router = express.Router();
 
+import asyncHandler from '../middleware/asynchandler.js';
+
 import products from '../data/products.js';
 
-router.get("/", (req, res) => {
-  res.send(products);
-});
+import Product from '../models/productModel.js';
 
-router.get("/:id", (req, res) => {
-  const product = products.find((p) => p.id === req.params.id);
-  res.send(product);
-})
+router.get(
+	'/',
+	asyncHandler(async (req, res) => {
+		const products = await Product.find({});
+		res.json(products);
+	}),
+);
+
+router.get(
+	'/:id',
+	asyncHandler(async (req, res) => {
+		const product = await Product.findById(req.params.id);
+
+		if (product) {
+			return res.json(product);
+		}
+		res.status(404).json({ message: 'Product not found' });
+	}),
+);
 
 export default router;
