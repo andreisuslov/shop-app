cfafb0c3d8899d4afc179cb66a4f20b4a5e48a03
andreisuslov <truvord@gmail.com>
2024-04-25 12:48:37 -0400

    Refactor HomeScreen and ProductScreen to use RTK Query

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,35 +1,28 @@
-import { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { useGetProductsQuery } from '../slices/productSlice';
 import Product from '../components/Product';
-import axios from 'axios';
 
 const HomeScreen = () => {
-	const [products, setProducts] = useState([]);
-
-	/* In React, we use useEffect() to run code when a component mounts or unmounts. 
-	In this case, we use it to fetch products from the API and set them in the state. 
-	An empty array as the second argument ensures that the effect runs only once when the component mounts.
-	*/
-	useEffect(() => {
-		const fetchProducts = async () => {
-			const { data } = await axios.get('/api/products');
-			setProducts(data);
-		};
-
-		fetchProducts();
-	}, []);
+	const { data: products, isLoading, error } = useGetProductsQuery();
 
 	return (
 		<>
-			<h1>Latest Products</h1>
-			<Row>
-				{/* Now the variable products pertains to this piece of state, so we can map over it. */}
-				{products.map((product) => (
-					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-						<Product product={product} />
-					</Col>
-				))}
-			</Row>
+			{isLoading ? (
+				<div>Loading...</div>
+			) : error ? (
+				<div>{error?.data.message || error.error}</div>
+			) : (
+				<>
+					<h1>Latest Products</h1>
+					<Row>
+						{products.map((product) => (
+							<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+								<Product product={product} />
+							</Col>
+						))}
+					</Row>
+				</>
+			)}
 		</>
 	);
 };
---
+++ frontend/src/slices/productSlice.js
@@ -0,0 +1,34 @@
+import { PRODUCTS_URL } from '../constants';
+import { apiSlice } from './apiSlice';
+
+/*
+ * In this section, we are injecting endpoints into the apiSlice.
+ * Injecting means that we are adding endpoints to the apiSlice,
+ * which serves as a container for all the endpoints.
+ * The builder object is used to create the endpoints.
+ */
+export const productSlice = apiSlice.injectEndpoints({
+	endpoints: (builder) => ({
+		getProducts: builder.query({
+			query: () => ({
+				url: PRODUCTS_URL,
+			}),
+			keepUnusedDataFor: 5,
+		}),
+		getProductDetails: builder.query({
+			query: (productId) => ({
+				url: `${PRODUCTS_URL}/${productId}`,
+			}),
+			keepUnusedDataFor: 5,
+		}),
+	}),
+});
+
+/*
+ * Here, we export the endpoints from the apiSlice using a specific convention style.
+ * The exported endpoint is named `useGetProductsQuery`, where:
+ * - The prefix 'use' is added to the name of the endpoint.
+ * - The suffix 'Query' is appended if the endpoint is a query (i.e., fetching data).
+ * - If the endpoint is a mutation (i.e., modifying data), the suffix 'Mutation' is used instead.
+ */
+export const { useGetProductsQuery, useGetProductDetailsQuery } = productSlice;
