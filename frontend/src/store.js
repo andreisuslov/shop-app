bf6d09f6771eb0d91b935951000377e0f840e7ed
andreisuslov <truvord@gmail.com>
2024-04-25 11:23:34 -0400

    Set up Redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,15 @@
 // this file is going to be an entry point for redux
 
 import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from './slices/apiSlice';
 
 const store = configureStore({
-	reducer: {},
+	reducer: {
+		[apiSlice.reducerPath]: apiSlice.reducer,
+	},
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(apiSlice.middleware),
+	devTools: true,
 });
 
 export default store;
