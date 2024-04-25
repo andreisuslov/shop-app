e99aecb90bb92e27d33f6632d0aad5f557a9618e
andreisuslov <truvord@gmail.com>
2024-04-25 10:54:05 -0400

    Add constants for URLs in frontend project

---
+++ frontend/src/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:5500' : '';
+export const PRODUCTS_URL = '/products';
+export const USERS_URL = '/users';
+export const ORDERS_URL = '/orders';
+export const PAYPAL_URL = '/api/config/paypal';
