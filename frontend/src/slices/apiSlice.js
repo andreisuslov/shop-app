bc10ffe7f1e21012cfbbce9697200c92d2b1ffe5
andreisuslov <truvord@gmail.com>
2024-04-25 11:47:00 -0400

    Add comments to code

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,10 +1,27 @@
+// Import the createApi and fetchBaseQuery functions from the '@reduxjs/toolkit/query/react' package
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// Import the BASE_URL constant from the '../constants' file
 import { BASE_URL } from '../constants';
 
+// Create a base query using fetchBaseQuery with the BASE_URL as the base URL for API requests
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
+/*
+ * API Slice is a container for all API endpoints.
+ * Currently, it is empty because we don't have any endpoints.
+ * We will add endpoints in the future.
+ * The 'builder' object is used to create endpoints.
+ */
 export const apiSlice = createApi({
+	// Use the baseQuery as the base query for the API slice
 	baseQuery,
+
+	// Define the tag types for the API slice
+	// Tag types are used for caching and invalidation of API responses
 	tagTypes: ['Product', 'Order', 'User'],
+
+	// Define the endpoints for the API slice using the 'builder' object
+	// Currently, there are no endpoints defined (an empty object is returned)
 	endpoints: (builder) => ({}),
 });
